2d106ed77f667228b066ac0fc6974729d11c0f12
David Grayston <david_grayston@dennis.co.uk>
2018-09-07 00:05:35 +0100

    Cleaning up unrelated/redundant methods

--- src/Driver/Drupal8/ParagraphsDriver.php
+++ src/Driver/Drupal8/ParagraphsDriver.php
@@ -6,10 +6,4 @@ use DennisDigital\Behat\Drupal\Paragraphs\Driver\ParagraphsDriverInterface;
  * ParagraphsDriver
  */
 class ParagraphsDriver implements ParagraphsDriverInterface {
-  /**
-   * @inheritDoc
-   */
-  public function updateDomain($domain, $valid) {
-    return paragraphs_known_domains_update($domain, $valid);
-  }
 }
--- src/Driver/ParagraphsDriverInterface.php
+++ src/Driver/ParagraphsDriverInterface.php
@@ -5,10 +5,4 @@ namespace DennisDigital\Behat\Drupal\Paragraphs\Driver;
  * ParagraphsDriverInterface
  */
 interface ParagraphsDriverInterface {
-  /**
-   * @param string $domain
-   * @param bool $valid
-   * @return bool
-   */
-  public function updateDomain($domain, $valid);
 }
